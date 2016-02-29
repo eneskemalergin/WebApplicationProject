ca69cfc91d4ff14353abace4ab8b558b900da822
eneskemalergin <eneskemalergin@gmail.com>
2016-02-29 13:44:13 -0600

    WebApp by Flask added

---
+++ flaskApp/app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, json, request, redirect, session, jsonify, url_for
+from flask.ext.mysql import MySQL
+from werkzeug import generate_password_hash, check_password_hash
+from werkzeug.wsgi import LimitedStream
+import os
+import uuid
+
+
+app = Flask(__name__)
+app.secret_key = "secret"
+
+
+# Main Page:
+@app.route('/')
+def main():
+    return render_template('index.html')
